d99ada948d44c6371fffba4bceb4f5456a5c8cb5
pgellert <gellpn22@gmail.com>
2016-05-17 20:36:03 +0200

    Details fragment added

---
+++ app/src/main/java/com/aporsoftware/lostpet/DetailsFragment.java
@@ -0,0 +1,19 @@
+package com.aporsoftware.lostpet;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+
+
+public class DetailsFragment extends Fragment {
+
+    public DetailsFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
